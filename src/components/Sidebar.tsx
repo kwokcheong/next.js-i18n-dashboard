9a6690e5d8348b70fb9e286d9ff958d4e9f56d11
Kwok Cheong <wongkwokcheong@hotmail.com>
2025-04-12 13:27:17 +0700

    update prescription page:

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,7 +13,7 @@ export default async function Sidebar({ locale }: Props) {
   return (
     <div className="fixed top-0 left-0 z-50 w-60 h-screen bg-gray-800 hidden lg:block">
       <div className="flex items-center justify-start h-16 w-[calc(100%-2rem)] mx-4">
-        <div className="text-xl text-gray-200">Starter App</div>
+        <div className="text-xl text-gray-200">GP Clinic System</div>
       </div>
 
       <hr className="h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25" />
